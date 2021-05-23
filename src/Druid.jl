a9416eed6784025ac6ce0fa3ac5865c4af61f57d
Mihir Gadgil <mmgadgil20@gmail.com>
2021-05-22 19:34:52 -0600

    Separate out client in its own file

--- src/Druid.jl
+++ src/Druid.jl
@@ -12,30 +12,7 @@ using JSON
 
 export Client, execute
 
-"""
-    Client
-
-    Client(url::URIs.URI)
-    Client(url::AbstractString)
-    Client(host, port, endpoint, scheme)
-
-Druid SQL query endpoint connection information store.
-
-#Examples
-```julia-repl
-julia> client = Client("http://localhost:8888/druid/v2/sql")
-Client(URI("http://localhost:8888/druid/v2/sql"))
-
-julia> client = Client("localhost", 8082, "druid/v2/sql", "http")
-Client(URI("http://localhost:8082/druid/v2/sql"))
-```
-"""
-struct Client
-    url::URI
-end
-Client(host, port, endpoint, scheme) = 
-    Client(joinpath(URI(;scheme=scheme, host=host, port=port), endpoint))
-Client(url::AbstractString) = Client(URI(url))
+include("client.jl")
 
 """
     execute(client, query; resultFormat="object", header=false, context=Dict(), parameters=[])
