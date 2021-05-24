827055f21289a809fcdc5eedcfe786b3322b4802
Mihir Gadgil <mmgadgil20@gmail.com>
2021-05-24 01:57:56 -0600

    Multiple aggregations

--- src/queries.jl
+++ src/queries.jl
@@ -6,7 +6,7 @@ mutable struct Timeseries <: Query
     intervals::Vector{Interval}
     granularity::Granularity
     filter
-    aggregations::Aggregator
+    aggregations::Vector{Aggregator}
     postAggregations
     descending
     limit
