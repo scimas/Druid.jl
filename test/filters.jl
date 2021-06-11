050986ff2fa5e330c376d99bf6f8e927dec4b311
Mihir Gadgil <mmgadgil20@gmail.com>
2021-06-10 23:23:13 -0600

    Fix filter tests

--- test/filters.jl
+++ test/filters.jl
@@ -1,30 +1,30 @@
 @testset "Contains" begin
-    @test_throws TypeError Contains("a", case_sensitive=1)
+    @test_throws TypeError(:typeassert, "", Bool, 1) Contains("a", case_sensitive=1)
 end
 
 @testset "Fragment" begin
-    @test_throws TypeError Fragment("a", case_sensitive=1)
+    @test_throws TypeError(:typeassert, "", Bool, 1) Fragment("a", case_sensitive=1)
 end
 
 @testset "SearchF" begin
-    @test_throws TypeError SearchF("dimensions", Contains("a"), extractionFn=1)
+    @test_throws TypeError(:typeassert, "", ExtractionFunction, 1) SearchF("dimensions", Contains("a"), extractionFn=1)
 end
 
 @testset "Like" begin
-    @test_throws TypeError Like("dimension", "pattern", escape=1)
-    @test_throws TypeError Like("dimension", "pattern", extractionFn=1)
+    @test_throws TypeError(:typeassert, "", AbstractString, 1) Like("dimension", "pattern", escape=1)
+    @test_throws TypeError(:typeassert, "", ExtractionFunction, 1) Like("dimension", "pattern", extractionFn=1)
 end
 
 @testset "Bound" begin
-    @test_throws TypeError Bound("dimension", lower=1)
-    @test_throws TypeError Bound("dimension", upper=1)
+    @test_throws TypeError(:typeassert, "", String, 1) Bound("dimension", lower=1)
+    @test_throws TypeError(:typeassert, "", String, 1) Bound("dimension", upper=1)
     @test_throws ErrorException("At least one of lower and upper must be specified") Bound("diimension")
-    @test_throws TypeError Bound("dimension", lower="a", lowerStrict=1)
-    @test_throws TypeError Bound("dimension", lower="a", upperStrict=1)
-    @test_throws TypeError Bound("dimension", lower="a", ordering=1)
-    @test_throws TypeError Bound("dimension", lower="a", extractionFn=1)
+    @test_throws TypeError(:typeassert, "", Bool, 1) Bound("dimension", lower="a", lowerStrict=1)
+    @test_throws TypeError(:typeassert, "", Bool, 1) Bound("dimension", lower="a", upperStrict=1)
+    @test_throws TypeError(:typeassert, "", String, 1) Bound("dimension", lower="a", ordering=1)
+    @test_throws TypeError(:typeassert, "", ExtractionFunction, 1) Bound("dimension", lower="a", extractionFn=1)
 end
 
 @testset "IntervalF" begin
-    @test_throws TypeError IntervalF("dimension", [Interval("a", "b")], extractionFn=1)
+    @test_throws TypeError(:typeassert, "", ExtractionFunction, 1) IntervalF("dimension", [Interval("a", "b")], extractionFn=1)
 end
